a43da4726035edd9e25ca3651d23367f97e88f57
Moiz Ali <moizmughalali@gmail.com>
2026-03-26 17:47:20 +0500

    just remove the BSC

--- index.js
+++ index.js
@@ -156,12 +156,12 @@ const NETWORKS = [
     rpc: process.env.ETH_WSS_URL,
     factory: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
   }, // Uniswap V2
-  {
-    name: "BSC",
-    id: 56,
-    rpc: process.env.BSC_WSS_URL,
-    factory: "0xcA143Ce32Fe78f1f7019d7d551a6402fc5350c73",
-  },
+  // {
+  //   name: "BSC",
+  //   id: 56,
+  //   rpc: process.env.BSC_WSS_URL,
+  //   factory: "0xcA143Ce32Fe78f1f7019d7d551a6402fc5350c73",
+  // },
   {
     name: "Base",
     id: 8453,
